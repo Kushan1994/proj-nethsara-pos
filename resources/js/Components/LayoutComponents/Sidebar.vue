ed60f45a50b3fc026317059680e036eeaf9732e0
wscreation <69392043+Websolcr@users.noreply.github.com>
2021-12-07 21:01:16 +0530

    first commit

--- resources/js/Components/LayoutComponents/Sidebar.vue
+++ resources/js/Components/LayoutComponents/Sidebar.vue
@@ -11,7 +11,7 @@
         </div>
         <div class="space-y-1">
           <h3 class="px-3 text-xs font-semibold text-gray-500 uppercase tracking-wider" id="projects-headline">
-            Projects
+            Projects1
           </h3>
           <div class="space-y-1" role="group" aria-labelledby="projects-headline">
             <a v-for="item in secondaryNavigation" :key="item.name" :href="item.href" class="group flex items-center px-3 py-2 text-sm font-medium text-gray-600 rounded-md hover:text-gray-900 hover:bg-gray-50">
