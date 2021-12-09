913bc0923fa1c6a49e8c2a35e5782a15dfbddd2c
wscreation <69392043+Websolcr@users.noreply.github.com>
2021-12-09 19:37:27 +0530

    sidebar

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,9 @@ require('./bootstrap');
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/inertia-vue3';
 import { InertiaProgress } from '@inertiajs/progress';
+import { Head, Link } from '@inertiajs/inertia-vue3';
+
+
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
@@ -12,6 +15,7 @@ createInertiaApp({
     setup({ el, app, props, plugin }) {
         return createApp({ render: () => h(app, props) })
             .use(plugin)
+            .component('InertiaLink', Link)
             .mixin({ methods: { route } })
             .mount(el);
     },
