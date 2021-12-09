050ca8b6a5a513d243e22e477b7298d834b72c4a
wscreation <69392043+Websolcr@users.noreply.github.com>
2021-12-09 19:37:27 +0530

    sidebar

---
+++ resources/js/Components/SvgIcon.vue
@@ -0,0 +1,38 @@
+<template>
+  <svg-vue
+    :fill="fill"
+    :icon="icon"
+    :stroke="stroke"
+    :stroke-width="strokeWidth"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  >
+  </svg-vue>
+</template>
+
+<script setup>
+defineProps({
+  icon: {
+    type: String,
+    required: true,
+  },
+
+  strokeWidth: {
+    type: String,
+    required: false,
+    default: '2',
+  },
+
+  stroke: {
+    type: String,
+    required: false,
+    default: 'currentColor',
+  },
+
+  fill: {
+    type: String,
+    required: false,
+    default: 'none',
+  },
+})
+</script>
