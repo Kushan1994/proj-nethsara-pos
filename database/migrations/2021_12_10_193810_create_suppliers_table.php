a40d6d49d2429ed30acbbbf1f9dac2187940d9e4
Suresh Hemal <53757686+sureshhemal@users.noreply.github.com>
2021-12-11 01:44:57 +0530

    end-no-edit

---
+++ database/migrations/2021_12_10_193810_create_suppliers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSuppliersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('suppliers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('contact_no_1');
+            $table->string('contact_no_2')->nullable();
+            $table->text('address');
+            $table->longText('notes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('suppliers');
+    }
+}
